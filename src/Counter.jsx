1fd4c4e31b459d6abc7e680954ea30fef9861b59
Manish <manishshekhawat756@gmail.com>
2025-05-08 11:36:49 +0530

    initial commit

---
+++ src/Counter.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function Counter() {
+  
+    const [count,setCount]=useState(0);
+
+    const incrementCount=()=>{
+        setCount(count+1);
+    }
+
+    const decrementCount=()=>{
+        setCount(count-1);
+    }
+    return (
+      <>
+        <h1>Counter App</h1>
+        <p>Count: {count}</p>
+        <button type="button" onClick={incrementCount}>increment</button>
+        <button type="button" onClick={decrementCount}>decrement</button>
+      </>
+    )
+  }
+  
+  export default Counter;
+  
